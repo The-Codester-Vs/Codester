3b22ddc3072b891c0050e7f23123ae52d851dcb1
The-Codester-Vs <viveksharma55236@gmail.com>
2020-12-03 14:55:29 +0530

    done

--- file1.py
+++ file1.py
@@ -4,3 +4,21 @@ punchline = "I am So Famous"
 print(f"The joke is:{joke}")
 input()
 print(f"Puchline is:{punchline}")
+
+
+password = "123"
+pw = ""
+count = 0
+max_count = 5
+auth = False
+
+while pw != password:
+    count = count + 1
+    if count > max_count:
+        break
+    pw = input(f"{count}:What is the password?")
+else:
+    auth = True
+
+print("Authorized "if auth else"Account Locked")        
+     
